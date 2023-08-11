217f7c9fcca42804cfaad795d084bf3b07176fb7
MykytaSoftDev <mykyta.naz.prog@gmail.com>
2023-08-11 19:44:56 -0400

    Base code

---
+++ ecommerce/store/views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .models import *
+import json
+import datetime
+# Create your views here.
+
+
+def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cart_items = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping':False}
+        cart_items = order['get_cart_items']
+
+    products = Product.objects.all()
+    contex = {'products': products, 'cart_items': cart_items}
+    return render(request, 'store/store.html', contex)
+
+
+def cart(request):
+    context = get_data(request)
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    context = get_data(request)
+    return render(request, 'store/checkout.html', context)
+
+
+def get_data(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+            customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cart_items = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cart_items = order['get_cart_items']
+
+    context = {'items': items, 'order': order, 'cart_items': cart_items}
+    return context
+
+def update_item(request):
+    data = json.loads(request.body)
+    product_id = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('ProductID:', product_id)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=product_id)
+    order, created = Order.objects.get_or_create(
+        customer=customer, complete=False)
+    order_item, created = OrderItem.objects.get_or_create(
+        order=order, product=product)
+
+    if action == 'add':
+        order_item.quantity = order_item.quantity + 1
+    elif action == 'remove':
+        order_item.quantity = order_item.quantity - 1
+
+    order_item.save()
+
+    if order_item.quantity <= 0:
+        order_item.delete()
+
+    return JsonResponse('Item was added', safe=False)
+
+
+def process_order(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(
+        customer=customer, complete=False)
+        total = data['form']['total']
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+    else:
+        print('User is not logged in...')
+    return JsonResponse('Payment complete!...', safe=False)
